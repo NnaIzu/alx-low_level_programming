dad54c33cedcf50e46e65a6903e39714bc9f7cae
NnaIzu <nna.victor@nigerianbar.ng>
2023-06-19 19:02:43 -0700

    Upper and lowercase combo

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char y;
+	char b;
+
+	y = 'a';
+	b = 'A';
+	while
+		(y <= 'z') {
+			putchar(y);
+			y++;
+		}
+	while
+		(b <= 'Z') {
+			putchar (b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
