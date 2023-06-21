6e6e02c5a8ea3e78dcdb2c36bc0532ce52c89661
NnaIzu <nna.victor@nigerianbar.ng>
2023-06-21 15:02:58 -0700

    Addition of Numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sums up two integers
+ * @i: first integers to be summed up
+ * @k: second integer to be summed with the first
+ * Return: value of the addition
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
