60a43b497869091507d18a3922dd933bff33224b
NnaIzu <nna.victor@nigerianbar.ng>
2023-06-20 22:41:24 -0700

    Lowercase Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
