fd2effe7278dc36e789ac1693c037bf26c3e1489
NnaIzu <nna.victor@nigerianbar.ng>
2023-07-26 21:34:47 -0700

    More fucntions and nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numnber
+ *
+ * Return: (void)
+ */
+
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (!(c == '2' || c == '4'))
+		_putchar(n);
+	}
+	_putchar('\n');
+}
+
